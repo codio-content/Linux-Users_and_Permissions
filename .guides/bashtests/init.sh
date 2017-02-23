311c4db448008017245e403aa7e3e167e41195a0
maximka <maxim.kraev@gmail.com>
2017-02-23 16:53:35 +0000

    fixes

--- .guides/bashtests/init.sh
+++ .guides/bashtests/init.sh
@@ -6,4 +6,3 @@ export BASHDIR=$HOME/workspace/.guides
 shopt -s histappend
 export PROMPT_COMMAND="history -a; history -c; history -r; $PROMPT_COMMAND"
 chmod +x "${BASHDIR}/bashtests/workspace-reset.sh"
-
--- .guides/content/End-of-unit-f94c.md
+++
@@ -1,3 +0,0 @@
-System security is an important asset as a Linux system manager.
-
-In the next unit, you will learn how to manage system processes through the CLI.
